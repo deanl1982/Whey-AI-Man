f186b28f6eb51ad967596d7e8b5d770d3bfdf6a2
TypicalLawrence <dal08082@gmail.com>
2025-10-09 19:45:20 +0100

    Updated TF provider

--- 01-Build-N8N/n8n-vm.tf
+++ 01-Build-N8N/n8n-vm.tf
@@ -1,6 +1,14 @@
-# Provider configuration
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
+  }
+}
+
 provider "azurerm" {
-    features {}
+  features {}
 }
 
 # Variables
